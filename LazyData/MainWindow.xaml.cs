d96fe982ce2209c22c15393b9f54b7507039ca23
beto-rodriguez <rodriguezo.alberto@gmail.com>
2015-12-19 17:34:46 -0600

    lazy data

---
+++ LazyData/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using LC_Demo;
+
+namespace LazyData
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private readonly MainWindowViewModel _viewModel;
+        public MainWindow()
+        {
+            _viewModel = new MainWindowViewModel();
+
+            InitializeComponent();
+
+            DataContext = _viewModel;
+        }
+
+        private void FetchDataOnClick(object sender, RoutedEventArgs e)
+        {
+            //I don´t know why the binding is not working correcty.
+            //this line fixes is but I really dont know why it does not detects the change by itself,
+            //SecondaryAxis.Labels is a dependency property
+            //note: this breaks MVVM
+
+            SecondaryAxis.Labels = new[] { "w", "x", "y", "z" };
+
+            //this only works when called twice
+            //SecondaryAxis.Labels = _viewModel.DummyLabels;
+
+            _viewModel.FetchData();
+        }
+    }
+}
---
+++ LazyData/MainWindowViewModel.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections.ObjectModel;
+using System.ComponentModel;
+using LiveCharts;
+
+namespace LC_Demo
+{
+    public class MainWindowViewModel : INotifyPropertyChanged
+    {
+        public MainWindowViewModel()
+        {
+            //Expected behavior: I should not be forced to initialize empty collections
+            //_dummySeries = new ObservableCollection<Series> ();
+            //_dummyLabels = new[] { "" };
+
+            //Fixed
+        }
+
+        public void FetchData()
+        {
+            //labels comes from data too, so I expect to be refreshed with databinding
+            DummyLabels = new[] { "w", "x", "y", "z" };
+            
+            //my data comes from a DB
+            DummySeries = new ObservableCollection<Series>
+            {
+                 new LineSeries
+                {
+                    Title = "Demo",
+                    PrimaryValues = new ObservableCollection<double>(new[] {2d, 4, 7, 1, 5})
+                }
+            };
+        }
+
+        private ObservableCollection<Series> _dummySeries;
+        public ObservableCollection<Series> DummySeries
+        {
+            get { return _dummySeries; }
+            set
+            {
+                _dummySeries = value;
+                OnPropertyChanged("DummySeries");
+            }
+        }
+
+        private string[] _dummyLabels;
+        public string[] DummyLabels
+        {
+            get { return _dummyLabels; }
+            set
+            {
+                _dummyLabels = value;
+                OnPropertyChanged("DummyLabels");
+            }
+        }
+
+        #region INotifyPropertyChanged
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged(string propertyName)
+        {
+            var handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
