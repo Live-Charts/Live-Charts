ea16ae576841f9c6df9297e64b5a284f367311d5
beto rodriguez <rodriguezo.alberto@gmail.com>
2016-03-26 21:53:34 -0600

    added unit test for #104

---
+++ UnitTesting/PieChartTest/PieSumEqualsToZero.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using LiveCharts;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTesting.PieChartTest
+{
+    [TestClass]
+    public class PieSumEqualsToZero
+    {
+        [TestMethod, TestCategory("PieChart")]
+        public void SumsZero()
+        {
+            var pieChart = new PieChart
+            {
+                Series = new SeriesCollection
+                {
+                    new PieSeries
+                    {
+                        Values = new ChartValues<double>
+                        {
+                            0,
+                            0,
+                            0,
+                            0
+                        }
+                    }
+                }
+            };
+            pieChart.UnsafeUpdate();
+        }
+    }
+}
