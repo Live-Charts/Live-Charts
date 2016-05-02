d22e91095d3ecf8f20e3b57a551f9991799f373e
beto rodriguez <rodriguezo.alberto@gmail.com>
2016-05-02 14:57:30 -0500

    Revert "version update"
    This reverts commit b1bee592ec974345dc4532db1deaf553d95a14c4.

--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -1,5 +1,7 @@
 ﻿using System.Resources;
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -24,5 +26,5 @@ using System.Reflection;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.0.0")]
-[assembly: AssemblyFileVersion("0.7.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
--- WpfView/Properties/AssemblyInfo.cs
+++ WpfView/Properties/AssemblyInfo.cs
@@ -1,4 +1,6 @@
 ﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using System.Windows;
 
