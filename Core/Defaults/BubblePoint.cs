24cc62cd388721ad5658372cc8d8130d10266c03
beto rodriguez <rodriguezo.alberto@gmail.com>
2016-07-05 17:07:44 -0500

    heat point for documentation

--- Core/Defaults/BubblePoint.cs
+++ Core/Defaults/BubblePoint.cs
@@ -117,31 +117,4 @@ namespace LiveCharts.Defaults
             if (PointChanged != null) PointChanged.Invoke();
         }
     }
-
-    /// <summary>
-    /// An already configured weighted chart point, this class notifies the chart to update every time a property changes
-    /// </summary>
-    public class HeatPoint : BubblePoint
-    {
-        /// <summary>
-        /// Initializes a new instance of HeatPoint class
-        /// </summary>
-        public HeatPoint()
-        {
-
-        }
-        
-        /// <summary>
-        /// _initializes a new instance of HeatPoint class, giving x, y and weight
-        /// </summary>
-        /// <param name="x"></param>
-        /// <param name="y"></param>
-        /// <param name="weight"></param>
-        public HeatPoint(double x, double y, double weight)
-        {
-            X = x;
-            Y = y;
-            Weight = weight;
-        }
-    }
 }
