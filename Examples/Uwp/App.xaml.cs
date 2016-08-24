0e1694837c9cb474d374f3af64ba42f4efa6100f
beto rodriguez <rodriguezo.alberto@gmail.com>
2016-08-23 19:41:34 -0500

    more settings

--- Examples/Uwp/App.xaml.cs
+++ Examples/Uwp/App.xaml.cs
@@ -15,7 +15,7 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-namespace Uwp
+namespace UWP
 {
     /// <summary>
     /// Provides application-specific behavior to supplement the default Application class.
--- Examples/Uwp/Properties/AssemblyInfo.cs
+++ Examples/Uwp/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Uwp")]
+[assembly: AssemblyTitle("UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Uwp")]
+[assembly: AssemblyProduct("UWP")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
