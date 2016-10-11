c75a3360044338f5f57547ced4923f9804e7a220
AuroraDysis <auroradysis@gmail.com>
2016-10-11 21:51:55 +0800

    add new samples (need to debug)

--- Examples/UWP/Commons/SampleDefinitions.cs
+++ Examples/UWP/Commons/SampleDefinitions.cs
@@ -5,6 +5,9 @@ using System.Text;
 using System.Threading.Tasks;
 using UWP.CartesianChart.Basic_Bars;
 using UWP.CartesianChart.Basic_Stacked_Bar;
+using UWP.CartesianChart.BasicLine;
+using UWP.CartesianChart.Bubbles;
+using UWP.CartesianChart.ConstantChanges;
 using UWP.Views;
 
 namespace UWP.Commons
@@ -17,7 +20,10 @@ namespace UWP.Commons
             new SampleDefinition("Basic Column", typeof(BasicColumn), SampleCategory.CartesianChart),
             new SampleDefinition("Basic Row", typeof(BasicRowExample), SampleCategory.CartesianChart),
             new SampleDefinition("Basic Stacked Column", typeof(BasicStackedColumnExample), SampleCategory.CartesianChart),
-            new SampleDefinition("Basic Stacked Row Percentage Example", typeof(BasicStackedRowPercentageExample), SampleCategory.CartesianChart),
+            new SampleDefinition("Basic Stacked Row Percentage", typeof(BasicStackedRowPercentageExample), SampleCategory.CartesianChart),
+            new SampleDefinition("Basic Line", typeof(BasicLineExample), SampleCategory.CartesianChart),
+            new SampleDefinition("Bubbles", typeof(BubblesExample), SampleCategory.CartesianChart),
+            new SampleDefinition("Constant Changes", typeof(ConstantChangesChart), SampleCategory.CartesianChart),
         };
     }
 }
---
+++ Examples/Uwp/CartesianChart/BasicLine/BasicLineExample.xaml.cs
@@ -0,0 +1,65 @@
+﻿using LiveCharts;
+using LiveCharts.Uwp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace UWP.CartesianChart.BasicLine
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class BasicLineExample : Page
+    {
+        public BasicLineExample()
+        {
+            this.InitializeComponent();
+
+            SeriesCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Series 1",
+                    Values = new ChartValues<double> { 4, 6, 5, 2 ,7 }
+                },
+                new LineSeries
+                {
+                    Title = "Series 2",
+                    Values = new ChartValues<double> { 6, 7, 3, 4 ,6 }
+                }
+            };
+
+            Labels = new[] { "Jan", "Feb", "Mar", "Apr", "May" };
+            YFormatter = value => value.ToString("C");
+
+            //modifing the series collection will animate and update the chart
+            SeriesCollection.Add(new LineSeries
+            {
+                Values = new ChartValues<double> { 5, 3, 2, 4 },
+                LineSmoothness = 0 //rect lines, 1 really smooth lines
+            });
+
+            //modifing any series values will also animate and update the chart
+            SeriesCollection[2].Values.Add(5d);
+
+            DataContext = this;
+        }
+
+        public SeriesCollection SeriesCollection { get; set; }
+        public string[] Labels { get; set; }
+        public Func<double, string> YFormatter { get; set; }
+    }
+}
