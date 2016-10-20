2b77fa8a351f021c67053c081115a8ad7e8637ee
beto rodriguez <rodriguezo.alberto@gmail.com>
2016-10-20 06:24:09 -0500

    fixes las fix :/

--- UwpView/Points/VerticalBezierPointView.cs
+++ UwpView/Points/VerticalBezierPointView.cs
@@ -113,7 +113,7 @@ namespace LiveCharts.Uwp.Points
 
             var animSpeed = chart.View.AnimationsSpeed;
 
-            //Segment.BeginPointAnimation(nameof(BezierSegment.Point1), Data.Point1.AsPoint(), animSpeed);
+            Segment.BeginPointAnimation(nameof(BezierSegment.Point1), Data.Point1.AsPoint(), animSpeed);
             Segment.BeginPointAnimation(nameof(BezierSegment.Point2), Data.Point2.AsPoint(), animSpeed);
             Segment.BeginPointAnimation(nameof(BezierSegment.Point3), Data.Point3.AsPoint(), animSpeed);
 
