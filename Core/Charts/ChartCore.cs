2153d9617b86309f1fe785cb98f4f84133901c5f
beto rodriguez <rodriguezo.alberto@gmail.com>
2017-01-10 20:47:52 -0600

    applying #341 fix to PCL

--- Core/Charts/ChartCore.cs
+++ Core/Charts/ChartCore.cs
@@ -201,8 +201,8 @@ namespace LiveCharts.Charts
                 SetAxisLimits(
                     AxisX[index],
                     View.ActualSeries
-                        .Where(series => series.Values != null && series.ScalesXAt == index)
-                        .ToArray(),
+                        // ReSharper disable once AccessToModifiedClosure
+                        .Where(series => series.Values != null && series.ScalesXAt == index),
                     AxisOrientation.X);
             }
 
@@ -211,8 +211,8 @@ namespace LiveCharts.Charts
                 SetAxisLimits(
                     AxisY[index],
                     View.ActualSeries
-                        .Where(series => series.Values != null && series.ScalesYAt == index)
-                        .ToArray(),
+                        // ReSharper disable once AccessToModifiedClosure
+                        .Where(series => series.Values != null && series.ScalesYAt == index),
                     AxisOrientation.Y);
             }
         }
@@ -334,6 +334,10 @@ namespace LiveCharts.Charts
             for (var index = 0; index < AxisY.Count; index++)
             {
                 var ax = AxisY[index];
+                var pr = ChartFunctions.FromPlotArea(ax.MaxPointRadius, AxisOrientation.Y, this, index) -
+                         ChartFunctions.FromPlotArea(0, AxisOrientation.Y, this, index);
+                ax.BotLimit += pr;
+                ax.TopLimit -= pr;
                 ax.UpdateSeparators(AxisOrientation.Y, this, index);
                 ax.View.SetTitleTop(curSize.Top + curSize.Height*.5 + ax.View.GetLabelSize().Width*.5);
             }
@@ -341,6 +345,10 @@ namespace LiveCharts.Charts
             for (var index = 0; index < AxisX.Count; index++)
             {
                 var xi = AxisX[index];
+                var pr = ChartFunctions.FromPlotArea(xi.MaxPointRadius, AxisOrientation.X, this, index) -
+                         ChartFunctions.FromPlotArea(0, AxisOrientation.X, this, index);
+                xi.BotLimit -= pr;
+                xi.TopLimit += pr;
                 xi.UpdateSeparators(AxisOrientation.X, this, index);
                 xi.View.SetTitleLeft(curSize.Left + curSize.Width*.5 - xi.View.GetLabelSize().Width*.5);
             }
@@ -669,7 +677,7 @@ namespace LiveCharts.Charts
         #endregion
 
         #region Privates
-        private static void SetAxisLimits(AxisCore ax, ISeriesView[] series, AxisOrientation orientation)
+        private static void SetAxisLimits(AxisCore ax, IEnumerable<ISeriesView> series, AxisOrientation orientation)
         {
             //                     [ max, min, pointRadius ]
             var boundries = new [] {double.MinValue, double.MaxValue, 0d};
