c9fcc8dd75ea145f85ab3a3f245316de7a700850
beto rodriguez <rodriguezo.alberto@gmail.com>
2017-02-12 17:51:01 -0600

    updated assembly version

--- Core/Properties/AssemblyInfo.cs
+++ Core/Properties/AssemblyInfo.cs
@@ -25,8 +25,8 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.2")]
-[assembly: AssemblyFileVersion("0.9.2")]
+[assembly: AssemblyVersion("0.9.3")]
+[assembly: AssemblyFileVersion("0.9.3")]
 
 [assembly: InternalsVisibleTo("LiveCharts.Wpf")]
 [assembly: InternalsVisibleTo("LiveCharts.Uwp")]
