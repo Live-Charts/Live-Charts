3bcd73dc6e09585f3c300c05d71c30bd19b8139f
beto rodriguez <rodriguezo.alberto@gmail.com>
2017-03-08 21:31:02 -0600

    updated assembly version

--- Core40/Properties/AssemblyInfo.cs
+++ Core40/Properties/AssemblyInfo.cs
@@ -34,8 +34,8 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.3.5")]
-[assembly: AssemblyFileVersion("0.9.3.5")]
+[assembly: AssemblyVersion("0.9.4")]
+[assembly: AssemblyFileVersion("0.9.4")]
 
 [assembly: InternalsVisibleTo("LiveCharts.Wpf")]
 [assembly: InternalsVisibleTo("LiveCharts.Uwp")]
