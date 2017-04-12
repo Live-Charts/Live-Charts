36c590a60619574714233857b6b5389a7ea325bc
Alberto Rodríguez <bto.rdz@hotmail.com>
2017-04-11 20:25:15 -0500

    license update

--- Core/PanningOptions.cs
+++ Core/PanningOptions.cs
@@ -1,6 +1,6 @@
 //The MIT License(MIT)
 
-//copyright(c) 2016 Alberto Rodriguez
+//Copyright(c) 2016 Alberto Rodriguez & LiveCharts Contributors
 
 //Permission is hereby granted, free of charge, to any person obtaining a copy
 //of this software and associated documentation files (the "Software"), to deal
