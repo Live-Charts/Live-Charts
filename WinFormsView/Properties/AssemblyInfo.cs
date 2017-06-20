c4962f9791747eeff0846d94f52ac44600c5a73e
Alberto Rodríguez <bto.rdz@hotmail.com>
2017-06-20 00:20:06 -0500

    updated assembly version

--- WinFormsView/Properties/AssemblyInfo.cs
+++ WinFormsView/Properties/AssemblyInfo.cs
@@ -32,7 +32,7 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.9.6")]
-[assembly: AssemblyFileVersion("0.9.6")]
+[assembly: AssemblyVersion("0.9.7")]
+[assembly: AssemblyFileVersion("0.9.7")]
 
 [assembly: AssemblyKeyFile(@"..\LVCSign.snk")]
